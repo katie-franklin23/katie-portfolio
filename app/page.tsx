d99323d95dbbf5f815520ea06d069d5eef92b702
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-11-15 12:57:23 +1300

    tailwind has been installed correctly

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import AboutSection from "@/components/AboutSection"
-import HeroSection from "@/components/HeroSection"
-import ProjectsSection from "@/components/ProjectsSection"
 export default function Home() {
   return (
-    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
-      <HeroSection />
-      <AboutSection />
-      <ProjectsSection />
+    <main>
+      <h1 className="text-2xl">Hello World</h1>
     </main>
   )
 }
