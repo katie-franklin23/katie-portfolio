bfe68e8d20e7d9f297de3d605907970f787675d3
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-11-15 16:39:59 +1300

    home id added to hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-"use client"
-import "../styles/globals.css"
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
-import { ThemeProvider } from "next-themes"
+'use client'
+import '../styles/globals.css'
+import Navbar from '@/components/Navbar'
+
+import Footer from '@/components/Footer'
+import { ThemeProvider } from 'next-themes'
 
 export default function RootLayout({
   children,
@@ -16,11 +17,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="dark:bg-stone-900">
+      <body>
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
           {children}
-          <Footer />
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from '@/components/HeroSection'
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-2xl">Hello World</h1>
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+      <HeroSection />
     </main>
   )
 }
