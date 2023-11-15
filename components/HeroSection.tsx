0ff9d214a2f6c38183cb926f2f9a16a390f499ff
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-11-15 17:04:34 +1300

    added headshot and blurb about myself

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HeroSection = () => {
   return (
     <section id="home">
       <div>
         <div>
-          <div></div>
+          <Image src="/headshotKT.JPG" alt="" width={300} height={300} />
+        </div>
+        <div>
+          <h1>Hi, I&#39;m Katie!</h1>
+          <p>
+            Based in Auckland, New Zealand. Looking for a Junior Developer role.
+            Passionate towards developing software solutions that enhance and
+            simplify life, adding depth and significance to everyday
+            experiences.
+          </p>
         </div>
       </div>
     </section>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,7 @@ const NAV_ITEMS: Array<NavItem> = [
 export default function Navbar() {
   const { systemTheme, theme, setTheme } = useTheme()
   const currentTheme = theme === 'system' ? systemTheme : theme
-  const pathname = usePathname()
+  // const pathname = usePathname()
   const [navbar, setNavbar] = useState(false)
   return (
     <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
