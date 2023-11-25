fdd409285be5d093674e57345ed071686f4f8744
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-11-25 13:51:36 +1300

    add github link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,11 @@ const Footer = () => {
           © 2023 Katie Franklin<a href="/" className="hover:underline"></a>
         </div>
         <div className="flex flex-row items-center justify-center space-x-2 mb-1">
-          <a href="https://github.com/hqasmei" rel="noreferrer" target="_blank">
+          <a
+            href="https://github.com/katie-franklin23"
+            rel="noreferrer"
+            target="_blank"
+          >
             <AiOutlineGithub
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
