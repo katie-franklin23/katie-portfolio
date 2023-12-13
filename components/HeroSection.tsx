e2d97d2449d5bcc9841061afba9d0c24f48a765a
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-12-13 22:34:11 +1300

    update json dependecies with @types/react versions

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
+import { Link } from 'react-scroll/modules'
 import { HiArrowDown } from 'react-icons/hi'
 
 const HeroSection = () => {
